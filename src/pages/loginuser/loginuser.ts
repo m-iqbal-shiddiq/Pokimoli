9a06a36acade9dad1f718ac5cc46c97be82b9a89
Raihan Nizar <raihannizar77@gmail.com>
2018-05-18 05:33:01 +0700

    startscreen sama login user udah bagus, tapi blom, tapi ga bisa lanjutin lagi karena page lainnya belum connect

--- src/pages/loginuser/loginuser.ts
+++ src/pages/loginuser/loginuser.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
-import { HomepagePage } from '../homepage/homepage';
 import { SignupuserPage } from '../signupuser/signupuser';
 
 /**
--- src/pages/signupuser/signupuser.ts
+++ src/pages/signupuser/signupuser.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomepagePage } from '../homepage/homepage';
 
 /**
  * Generated class for the SignupuserPage page.
