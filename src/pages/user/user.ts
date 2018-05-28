35f0abcf4e70ab75642d41cc535080fe6c289c78
miqbals1649 <muhammadiqbals.1998@gmail.com>
2018-05-28 11:30:29 +0700

    login bisa

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { SignupPage } from '../signup/signup';
+import { Http } from '@angular/http';
+import { Data } from '../../provider/data';
+
 
 /**
  * Generated class for the UserPage page.
@@ -16,16 +19,62 @@ import { SignupPage } from '../signup/signup';
   templateUrl: 'user.html',
 })
 export class UserPage {
+  email: any;
+  password: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadctrl: LoadingController,
+    public http: Http,
+    private data: Data,
+    public loadCtrl: LoadingController,
+    public alertCtrl: AlertController
+    ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserPage');
   }
 
-  gotoHome(){
-    this.navCtrl.push(HomePage);
+  signIn(){
+    if(this.email && this.password){
+      let loading = this.loadCtrl.create({
+        content: 'memuat..'
+      });
+
+      loading.present();
+
+      //apiPost
+      let input = {
+        email: this.email, 
+        password: this.password
+      };
+      console.log(input);
+      this.http.post(this.data.BASE_URL+"/login_user.php",input).subscribe(data => {
+      let response = data.json();
+      console.log(response); 
+      if(response.status==200){    
+        this.data.logout();
+        
+        this.data.login(response.data,"user");//ke lokal
+        
+        this.navCtrl.setRoot(HomePage);
+        loading.dismiss();
+      }
+      else {
+        loading.dismiss();
+          let alert = this.alertCtrl.create({
+            title: 'Login Gagal',      
+            message : 'Password dan Email tidak cocok',
+            buttons: ['OK']
+          });
+          alert.present();
+          
+      }    
+      });
+      //apiPost    
+    }
   }
 
   gotoSignup(){
