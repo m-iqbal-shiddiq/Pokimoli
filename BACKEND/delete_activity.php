f3cb14a019192e6a2bcbfa5ef0bf370fa6a8a73b
miqbals1649 <muhammadiqbals.1998@gmail.com>
2018-06-01 23:19:46 +0700

    delete

---
+++ BACKEND/delete_activity.php
@@ -0,0 +1,34 @@
+<?php
+    include 'db_connect.php';
+    $postdata = file_get_contents("php://input");
+    $id_activity = "";
+    if (isset($postdata)) {
+        $request = json_decode($postdata);
+        $id_activity = $request->id_activity;
+        if($request){
+            $query = mysqli_query($connect, "DELETE FROM activity WHERE id_activity = '$id_activity' ");
+            if($query){
+                $data =array(
+                    'message' => "Delete succesfully",
+                    'status' => "200"
+                );
+            }
+            else{
+                $data =array(
+                    'message' => "Failed!",
+                    'status' => "404"
+                );
+
+            }
+        }
+        else {
+            $data =array(
+                'message' => "No Data!",
+                'status' => "503"
+            );
+        }
+
+    }
+
+    echo json_encode($data);
+?>
--- BACKEND/terima_pesanan.php
+++ BACKEND/terima_pesanan.php
@@ -1,11 +1,11 @@
 <?php
 	include 'db_connect.php';
-	$q = mysqli_query($connect,"SELECT * FROM user JOIN activity");
+	$q = mysqli_query($connect,"SELECT user.nama_user, user.no_hp, activity.lokasi_penjemputan, activity.lokasi_tujuan, activity.keterangan FROM user JOIN activity ON activity.id_user = user.id_user WHERE activity.trig = '0'");
 	while($result=mysqli_fetch_assoc($q)){
 		$result_set[]=$result;
 	}
 	$data=array(
-		'message'=>'Get Data Health History Success',
+		'message'=>'Get Activity',
 		'data'=>$result_set,
 		'status'=>'200'
 	);
